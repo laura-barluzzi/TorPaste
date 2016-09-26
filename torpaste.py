4ebf1b80309f33f7f71c6fc098edde515ae10b35
DaKnOb <daknob.mac@gmail.com>
2016-09-26 18:16:26 +0300

    Handle all errors gracefully (Fix #16)

--- torpaste.py
+++ torpaste.py
@@ -11,12 +11,20 @@ VERSION = "0.3"
 
 @app.route('/')
 def index():
-    return render_template("index.html", title=WEBSITE_TITLE, version=VERSION)
+    return render_template(
+		"index.html",
+		title=WEBSITE_TITLE,
+		version=VERSION
+	)
 
 @app.route("/new", methods=["GET", "POST"])
 def newpaste():
 	if(request.method == "GET"):
-		return render_template("index.html", title=WEBSITE_TITLE, version=VERSION)
+		return render_template(
+			"index.html",
+			title=WEBSITE_TITLE,
+			version=VERSION
+		)
 	else:
 		if(request.form['content']):
 			PasteID = str(sha256(request.form['content']).hexdigest())
@@ -44,19 +52,51 @@ def newpaste():
 @app.route("/view/<pasteid>")
 def viewpaste(pasteid):
 	if(not pasteid.isalnum()):
-		return "<h1>Bad Request</h1>", 400
+		return Response(
+			render_template(
+				"index.html",
+				title=WEBSITE_TITLE,
+				version=VERSION,
+				error = "Invalid Paste ID. Please check the link you used or use Pastes button above."
+			),
+			400
+		)
 	if(len(pasteid) < 6):
-		return "<h1>Bad Request</h1>", 400
+		return Response(
+			render_template(
+				"index.html",
+				title=WEBSITE_TITLE,
+				version=VERSION,
+				error = "Paste ID too short. Usually Paste IDs are longer than 6 characters. Please make sure the link you clicked is correct or use the Pastes button above."
+			),
+			400
+		)
 	A = pasteid[0:2]
 	B = pasteid[2:4]
 	PastePath = "pastes/" + A + "/" + B + "/" + pasteid
 	if(not os.path.isfile(PastePath)):
-		return "<h1>Paste not found</h1>", 404
+		return Response(
+			render_template(
+				"index.html",
+				title=WEBSITE_TITLE,
+				version=VERSION,
+				error = "A paste with this ID could not be found. Sorry."
+			),
+			404
+		)
 	PasteContent = open(PastePath, "r").read()
 	PasteDate = open(PastePath + ".date", "r").read()
 	PasteDate = datetime.fromtimestamp(int(PasteDate) + time.altzone + 3600).strftime("%H:%M:%S %d/%m/%Y")
 	PasteSize = formatSize(len(PasteContent))
-	return render_template("view.html", content=PasteContent, date=PasteDate, size=PasteSize, pid=pasteid, title=WEBSITE_TITLE, version=VERSION)
+	return render_template(
+		"view.html",
+		content=PasteContent,
+		date=PasteDate,
+		size=PasteSize,
+		pid=pasteid,
+		title=WEBSITE_TITLE,
+		version=VERSION
+	)
 
 @app.route("/raw/<pasteid>")
 def rawpaste(pasteid):
@@ -70,13 +110,21 @@ def rawpaste(pasteid):
 	if(not os.path.isfile(PastePath)):
 		return "No such paste", 404
 	PasteContent = open(PastePath, "r").read()
-	return Response(PasteContent, mimetype="text/plain")
+	return Response(
+		PasteContent,
+		mimetype="text/plain"
+	)
 
 @app.route("/list")
 def list():
 	PasteList = []
 	if(len(os.listdir("pastes")) < 2):
-		return render_template("list.html", pastes=['none'], title=WEBSITE_TITLE, version=VERSION)
+		return render_template(
+			"list.html",
+			pastes=['none'],
+			title=WEBSITE_TITLE,
+			version=VERSION
+		)
 	for a in os.listdir("pastes"):
 		if(a.find(".") != -1):
 			continue
@@ -84,7 +132,12 @@ def list():
 			for paste in os.listdir("pastes/" + a + "/" + b):
 				if(paste.find(".") == -1):
 					PasteList.append(paste)
-	return render_template("list.html", pastes=PasteList, title=WEBSITE_TITLE, version=VERSION)
+	return render_template(
+		"list.html",
+		pastes=PasteList,
+		title=WEBSITE_TITLE,
+		version=VERSION
+	)
 
 # Functions
 def formatSize(size):

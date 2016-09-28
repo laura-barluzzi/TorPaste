ea08d08ca6e05c957e095302f64fc708cb123cd9
DaKnOb <daknob.mac@gmail.com>
2016-09-28 21:50:08 +0300

    Multiple Backends
    - Add code to handle multiple backends
    - Add filesystem backend compatible with existing solution
    - Add backend guidelines and example code
    - Clean up filesystem backend code

---
+++ backends/filesystem.py
@@ -0,0 +1,154 @@
+#!../bin/python
+
+import exceptions as e
+import os
+
+# initializeBackend()
+## This method is called when the Flask application starts. Here you can do
+## any initialization that may be required, such as connecting to a remote
+## database or making sure a file exists.
+
+def initializeBackend():
+	try:
+		os.mkdirs("pastes")
+	except:
+		pass
+	return
+
+# newPaste(pasteid, pastecontent)
+## This method is called when the Flask application wants to create a new
+## paste. The arguments given are the Paste ID, which is a paste identifier
+## that is not neccesarily unique, as well as the paste content. Please note
+## that the paste must be retrievable given the above Paste ID as well as
+## the fact that pasteid is (typically) an ASCII string while pastecontent
+## can (and will) contain UTF-8 characters.
+
+def newPaste(pasteid, pastecontent):
+	a = pasteid[0:2]
+	b = pasteid[2:4]
+
+	try:
+		os.makedirs("pastes/" + a + "/" + b)
+	except:
+		pass
+
+	ppath = "pastes/" + a + "/" + b + "/" + pasteid
+
+	try:
+		with open(ppath, "w+") as fd:
+			fd.write(pastecontent)
+	except:
+		raise e.ErrorException("An issue occured with the local filesystem. Please try again later. If the problem perists, try notifying a system administrator.")
+
+	return
+
+# updatePasteMetadata(pasteid, metadata)
+## This method is called by the Flask application to update a paste's
+## metadata. For this to happen, the application passes the Paste ID,
+## which is typically an ASCII string, as well as a Python dictionary
+## that contains the new metadata. This method must overwrite any and
+## all metadata with the passed dictionary. For example, if a paste has
+## the keys a and b and this method is called with only keys b and c,
+## the final metadata must be b and c only, and not a.
+
+def updatePasteMetadata(pasteid, metadata):
+	a = pasteid[0:2]
+	b = pasteid[2:4]
+
+	try:
+		os.makedirs("pastes/" + a + "/" + b)
+	except:
+		pass
+
+	ppath = "pastes/" + a + "/" + b + "/" + pasteid
+
+	try:
+		for k, v in metadata.iteritems():
+			with open(ppath + "." + k, "w+") as fd:
+				fd.write(v)
+	except:
+		raise e.ErrorException("An issue occured with the local filesystem. Please try again later. If the problem persists, try notifying a system administrator.")
+
+	return
+
+# doesPasteExist(pasteid)
+## This method is called when the Flask application wants to check if a
+## paste with a given Paste ID exists. The Paste ID is (typically) an
+## ASCII string and your method must return True if a paste with this ID
+## exists, or False if it doesn't.
+
+def doesPasteExist(pasteid):
+	a = pasteid[0:2]
+	b = pasteid[2:4]
+
+	return os.path.isfile("pastes/" + a + "/" + b + "/" + pasteid)
+
+# getPasteContents(pasteid)
+## This method must return all the paste contents in UTF-8 encoding for
+## a given Paste ID. The Paste ID is typically in ASCII, and it is
+## guaranteed that this Paste ID exists.
+
+def getPasteContents(pasteid):
+	a = pasteid[0:2]
+	b = pasteid[2:4]
+
+	try:
+		with open("pastes/" + a + "/" + b + "/" + pasteid) as fd:
+			return fd.read()
+	except:
+		raise e.ErrorException("An issue occured with the local filesystem. Please try again later. If the problem persists, try notifying a system administrator.")
+
+# getPasteMetadata(pasteid)
+## This method must return a Python Dictionary with all the currently
+## stored metadata for the paste with the given Paste ID. All keys of
+## the dictionary are typically in ASCII, while all values are in
+## UTF-8. It is guaranteed that the Paste ID exists.
+
+def getPasteMetadata(pasteid):
+	a = pasteid[0:2]
+	b = pasteid[2:4]
+
+	ret = {}
+
+	try:
+		for f in os.listdir("pastes/" + a + "/" + b + "/"):
+			if (pasteid in f) and ("." in f):
+				t = f.split(".")[1]	
+				with open("pastes/" + a + "/" + b + "/" + f, "r") as fd:
+					ret[t] = fd.read()
+	except:
+		raise e.ErrorException("An issue occured with the local filesystem. Please try again later. If the problem persists, try notifying a system administrator.")
+
+	if ( len(ret) == 0 ):
+		raise e.WarningException("Failed to load Paste Metadata. Some features like the paste date may not work. If the problem persists, try notifying a system administrator.")
+
+	return ret
+
+# getAllPasteIDs()
+## This method must return a Python list containing each and every
+## Paste ID, in ASCII. The order does not matter so it can be the same
+## or it can be different every time this function is called. In the
+## case of no pastes, the method must return a Python list with a
+## single item, whose content must be equal to 'none'.
+
+def getAllPasteIDs():
+	ret = []
+
+	try:
+		for i in os.listdir("pastes"):
+			if ( "." in i ):
+				continue
+			for j in os.listdir("pastes/" + i):
+				if ( "." in j ):
+					continue
+				for k in os.listdir("pastes/" + i + "/" + j):
+					if ( "." in k ):
+						continue
+					ret.append(k)
+		if ( len ( ret ) == 0 ):
+			return ['none']
+	
+		return ret
+	
+	except:
+		raise e.ErrorException("An issue occured with the local filesystem. Please try again later. If the problem persists, try notifying a system administrator.")
--- torpaste.py
+++ torpaste.py
@@ -4,6 +4,7 @@ from flask import *
 from hashlib import sha256
 from datetime import datetime
 import os, time
+import backends.filesystem as b
 app = Flask(__name__)
 
 WEBSITE_TITLE = "Tor Paste"
@@ -39,15 +40,33 @@ def newpaste():
 					page = "new",
 					error = "The current version of TorPaste supports ASCII characters only. UTF-8 support is coming soon."
 				)
-			A = PasteID[0:2]
-			B = PasteID[2:4]
 			try:
-				os.makedirs("pastes/" + A + "/" + B)
-			except:
-				pass
-			PASTEPATH = "pastes/" + A + "/" + B + "/" + PasteID
-			open(PASTEPATH, "w+").write(request.form['content'])
-			open(PASTEPATH + ".date", "w+").write(str(int(time.time())))
+				b.newPaste(PasteID, request.form['content'])
+			except b.e.ErrorException as errmsg:
+				return render_template(
+					"index.html",
+					title = WEBSITE_TITLE,
+					version = VERSION,
+					page = "new",
+					error = errmsg
+				)
+
+			try:
+				b.updatePasteMetadata(
+					PasteID,
+					{
+						"date": unicode(int(time.time()))
+					}
+				)
+			except b.e.ErrorException as errmsg:
+				return render_template(
+					"index.html",
+					title = WEBSITE_TITLE,
+					version = VERSION,
+					page = "new",
+					error = errmsg
+				)
+
 			return redirect("/view/" + PasteID)
 		else:
 			return Response(
@@ -85,10 +104,7 @@ def viewpaste(pasteid):
 			),
 			400
 		)
-	A = pasteid[0:2]
-	B = pasteid[2:4]
-	PastePath = "pastes/" + A + "/" + B + "/" + pasteid
-	if(not os.path.isfile(PastePath)):
+	if ( not b.doesPasteExist(pasteid) ):
 		return Response(
 			render_template(
 				"index.html",
@@ -99,8 +115,37 @@ def viewpaste(pasteid):
 			),
 			404
 		)
-	PasteContent = open(PastePath, "r").read()
-	PasteDate = open(PastePath + ".date", "r").read()
+
+	try:
+		PasteContent = b.getPasteContents(pasteid)
+	except b.e.ErrorException as errmsg:
+		return render_template(
+			"index.html",
+			title = WEBSITE_TITLE,
+			version = VERSION,
+			error = errmsg,
+			page = "new"
+		)
+
+	try:
+		PasteDate = b.getPasteMetadata(pasteid)['date']
+	except b.e.ErrorException as errmsg:
+		return render_template(
+			"index.html",
+			title = WEBSITE_TITLE,
+			version = VERSION,
+			error = errmsg,
+			page = "new"
+		)
+	except b.e.WarningException as errmsg:
+		return render_template(
+			"index.html",
+			title = WEBSITE_TITLE,
+			version = VERSION,
+			warning = errmsg,
+			page = "new"
+		)
+
 	PasteDate = datetime.fromtimestamp(int(PasteDate) + time.altzone + 3600).strftime("%H:%M:%S %d/%m/%Y")
 	PasteSize = formatSize(len(PasteContent))
 	return render_template(
@@ -120,12 +165,15 @@ def rawpaste(pasteid):
 		return "No such paste", 404
 	if(len(pasteid) < 6):
 		return "No such paste", 404
-	A = pasteid[0:2]
-	B = pasteid[2:4]
-	PastePath = "pastes/" + A + "/" + B + "/" + pasteid
-	if(not os.path.isfile(PastePath)):
+	if ( not b.doesPasteExist(pasteid) ):
 		return "No such paste", 404
-	PasteContent = open(PastePath, "r").read()
+	try:
+		PasteContent = b.getPasteContents(pasteid)
+	except b.e.ErrorException as errmsg:
+		return Response(
+			errmsg,
+			500
+		)
 	return Response(
 		PasteContent,
 		mimetype = "text/plain"
@@ -133,8 +181,18 @@ def rawpaste(pasteid):
 
 @app.route("/list")
 def list():
-	PasteList = []
-	if(len(os.listdir("pastes")) < 2):
+	try:
+		PasteList = b.getAllPasteIDs()
+	except b.e.ErrorException as errmsg:
+		return render_template(
+			"index.html",
+			title = WEBSITE_TITLE,
+			version = VERSION,
+			page = "new",
+			error = errmsg
+		)
+
+	if ( PasteList[0] == 'none' ):
 		return render_template(
 			"list.html",
 			pastes = ['none'],
@@ -142,13 +200,6 @@ def list():
 			version = VERSION,
 			page = "list"
 		)
-	for a in os.listdir("pastes"):
-		if(a.find(".") != -1):
-			continue
-		for b in os.listdir("pastes/" + a):
-			for paste in os.listdir("pastes/" + a + "/" + b):
-				if(paste.find(".") == -1):
-					PasteList.append(paste)
 	return render_template(
 		"list.html",
 		pastes = PasteList,
@@ -177,6 +228,13 @@ def formatSize(size):
 		else:
 			break
 	return str(round(size,1)) + " " + scales[count]
+
+# Required Initialization Code
+try:
+	b.initializeBackend()
+except:
+	print("Failed to initialize backend")
+	exit(1)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")

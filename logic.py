e83b3bf341b819580c92566424f03cc2b8752e8b
DaKnOb <daknob.mac@gmail.com>
2016-11-02 14:24:05 +0200

    Fix typos in logic.py

--- logic.py
+++ logic.py
@@ -48,7 +48,7 @@ def create_new_paste(content, metadata, config):
     try:
         paste_id = str(sha256(content.encode('utf-8')).hexdigest())
     except:
-        return "ERROR", "An issue occured while handling the paste. " +\
+        return "ERROR", "An issue occurred while handling the paste. " +\
             "Please try again later. If the problem persists, try " +\
             "notifying a system administrator."
 
